9b8f4d545eca41911a665798d8501c7b7370c148
IgnaGonzAvi <103262229+IgnaGonzAvi@users.noreply.github.com>
2022-05-01 15:08:34 -0400

    Add files via upload

---
+++ asincronico3.js
@@ -0,0 +1,20 @@
+//FizzBuzz costó pero se pudo
+var i=1;
+
+console.log("Jueguito FizzBuzz")
+
+while(i<=100){
+    if(i%15==0){
+        console.log("FizzBuzz")
+    }else if(i%3==0){
+        console.log("Fizz")
+    }else if(i%5==0){
+        console.log("Buzz")
+    }else{
+        console.log(i)
+    }
+    i++;
+} 
+
+
+//no supe como hacer el piedra papel y tijera
