cecd07110fa3c98b71a985ec0ef9f430c0b10116
IgnaGonzAvi <103262229+IgnaGonzAvi@users.noreply.github.com>
2022-05-08 23:16:20 -0400

    Add files via upload

---
+++ asincrónico6.js
@@ -0,0 +1,31 @@
+// peliculas
+
+var peliculas = [
+
+    {loHasVisto: "no viste", titulo: "el día despues de mañana -", rating: 2 + " estrellas"},
+    {loHasVisto: "viste", titulo: "titanic - ", rating: 4 + " estrellas"},
+    {loHasVisto: "viste", titulo: "matrix - ", rating: 5 + " estrellas", },
+    
+    ];
+
+    for (var peliculitas in peliculas){
+        console.log(peliculas[peliculitas]);
+    }
+
+    
+    //arreglo de objetos
+
+
+var arregloDeObjetos = [
+    {palabra : "hola", numero : 2},
+    {palabra : "chao", numero : 3},
+];
+
+console.log(arregloDeObjetos[0]);
+console.log(arregloDeObjetos[0]);
+console.log(arregloDeObjetos[1]);
+console.log(arregloDeObjetos[1]);
+console.log(arregloDeObjetos[1]);
+
+
+
